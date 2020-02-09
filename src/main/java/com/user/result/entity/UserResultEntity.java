4b9b79124ce8a3fe005a2dce6672a2b41ed94963
javaseekers <javaseekers9@gmail.com>
2020-02-09 20:25:31 +0530

    intial version

--- src/main/java/com/user/result/entity/UserResultEntity.java
+++ src/main/java/com/user/result/entity/UserResultEntity.java
@@ -1,12 +1,16 @@
 package com.user.result.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
+@Entity
+@Table(name="result")
 public @Data class UserResultEntity
 {
 	@Id
